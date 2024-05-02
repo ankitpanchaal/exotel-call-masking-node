12d9fe94a4b047a1bf69a77e46c6f91a5d71b7c7
ankitskilzenpachal <ankit.panchal@skilzen.com>
2024-05-02 16:25:26 +0530

    code backup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,7 +18,7 @@ app.get('/exotel-webhook', (req, res) => {
     const { CallFrom, CallTo } = req.query;
 
     // Customize your number masking logic here
-    const destinationNumber = "+917987058151"; // Destination number to mask
+    const destinationNumber = "+91798xx581xx"; // Destination number to mask
     const maskedResponse = {
         "fetch_after_attempt": false,
         "destination": {
@@ -40,6 +41,42 @@ app.get('/exotel-webhook', (req, res) => {
 
     // Send the masked response back to Exotel
     res.json(maskedResponse);
+});
+
+// Route to handle click-to-call requests
+app.post('/click-to-call', async (req, res) => {
+    try {
+        const { from, to, callerId } = req.body;
+
+        // Construct the request body for Exotel API
+        const requestBody = {
+            From: 919993673521,
+            To: 917054557586,
+            CallerId: "5555553625362563",
+            // StatusCallback: statusCallback,
+            StatusCallbackEvents: ['terminal'],
+            StatusCallbackContentType: 'application/json'
+        };
+
+        // Make a POST request to Exotel's API
+        const response = await axios.post(
+            'https://api.exotel.com/v1/Accounts/skilzen2/Calls/connect',
+            requestBody,
+            {
+                auth: {
+                    username: '85cb7d43da5f4ed28f20a7c6b3c934cf59df99b3a7bd400e',
+                    password: 'c1b9d09d21e09fd9c82d802fc5dd0376f306ca631f060106'
+                }
+            }
+        );
+
+
+        // Send the Exotel response back to the client
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error occurred:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
 // Start the Express server
